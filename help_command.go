ae7d0e959fdad560bee0d3aea1daf69b0677f949
Omar Salam <opace88@gmail.com>
2024-02-01 03:09:57 +0800

    first

---
+++ help_command.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func helpCommand() error {
+	fmt.Println()
+	fmt.Println("Welcome to Pokego!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
---
+++ repl.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("PokeGo >> ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+type Command struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]Command {
+	return map[string]Command{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    helpCommand,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the program",
+			callback:    exitCommand,
+		},
+	}
+}
