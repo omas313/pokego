f563f05b228d0ef3014cd5f262f69cd073dbcf40
Omar Salam <opace88@gmail.com>
2024-02-05 03:02:27 +0800

    feat: protect cache with mutex

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,9 +1,15 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]CacheEntry
+
+	// create mutex to protect the cache from concurrent access
+	mux *sync.Mutex
 }
 
 type CacheEntry struct {
@@ -14,12 +20,18 @@ type CacheEntry struct {
 func NewCache(duration time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]CacheEntry),
+		mux:   &sync.Mutex{},
 	}
 	go c.reapLoop(duration)
 	return c
 }
 
 func (c *Cache) Add(key string, value []byte) {
+	// lock the cache to prevent concurrent access
+	c.mux.Lock()
+	// unlock the cache when the function returns
+	defer c.mux.Unlock()
+
 	c.cache[key] = CacheEntry{
 		value:    value,
 		createAt: time.Now().UTC(),
@@ -27,6 +39,11 @@ func (c *Cache) Add(key string, value []byte) {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	// lock the cache to prevent concurrent access
+	c.mux.Lock()
+	// unlock the cache when the function returns
+	defer c.mux.Unlock()
+
 	cacheEntry, exists := c.cache[key]
 	return cacheEntry.value, exists
 }
@@ -36,6 +53,11 @@ func (c *Cache) Delete(key string) {
 }
 
 func (c *Cache) reap(duration time.Duration) {
+	// lock the cache to prevent concurrent access
+	c.mux.Lock()
+	// unlock the cache when the function returns
+	defer c.mux.Unlock()
+
 	cutoffTime := time.Now().UTC().Add(-duration)
 	for key, entry := range c.cache {
 		// if the entry is older than the cutoff time, delete it
